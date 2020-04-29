f26141a72abda33a46a70b25af2f0012a2739db9
atmunr <atmunr@gmail.com>
2020-04-29 16:27:46 -0300

    Create data points from a matrix

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -13,17 +13,34 @@
 export default {
   name: 'PageIndex',
   data () {
-    const x : Number[] = [1, 2, 3, 4, 5];
-    const y : Number[] = [2, 3, 4, 5, 6];
     let plotUrl : String = '';
-    return { x, y, plotUrl };
+    return { plotUrl };
+  },
+  computed : {
+    dataPoints: () => {
+      const calibData : Number[][] = [
+        [1, 1,  2,  3],
+        [2, 3,  4,  5],
+        [3, 5,  6,  7],
+        [4, 7,  8,  9],
+        [5, 9, 10, 11]
+      ];
+      let analytes : Number[] = [], signals : Number[] = [];
+      for (let row = 0; row < 5; row++) {
+        for (let col = 1; col < 4; col++) {
+          analytes.push(calibData[row][0]);
+          signals.push(calibData[row][col])
+        } 
+      }
+      return {analytes: analytes, signals: signals};
+    }
   },
   methods: {
     getPlot () {
       this.$axios.post('https://atmunr.ocpu.io/UNIVAR_EJCR_R-API/R/plotVectors', {
-        x: this.x, y: this.y,
+        x: this.dataPoints.analytes, y: this.dataPoints.signals,
         title: 'A plot', xlabel: 'Some values', ylabel: 'Some more values',
-        slope: 1, intercept: 1
+        slope: 2, intercept: 0
       })
       .then((response) => {
         this.plotUrl = 'https://cloud.opencpu.org' + response.data.split('\n')[4];
