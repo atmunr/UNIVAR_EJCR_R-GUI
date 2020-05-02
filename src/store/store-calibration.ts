40ad57477340e8e0ffa4912928e527b384a1edf1
atmunr <atmunr@gmail.com>
2020-05-02 19:16:05 -0300

    Refactor store logic
    Move logic from mutations to actions. Mutations should only accept a
    payload and do basic assignments.
    
    Move logic from actions outside. Actions should only do API calls,
    generate payloads, call mutations and dispatch other actions. Other
    logic is now in a seperate file.

--- src/store/store-calibration.ts
+++ src/store/store-calibration.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import axios from 'axios';
 
+import { createDataPoints, generalInfo } from './utils';
+
 const state = {
 
   calibrationSamples: [],
@@ -66,21 +68,23 @@ const mutations = {
 	},
 
 	updateRegressionValues (state, payload) {
-	  state.regression.slope.value = payload[0][0];
-	  state.regression.slope.deviation = payload[1][0];
-	  state.regression.slope.plusMinus = payload[2][0];
+	  state.regression.slope.value = payload.slope.value;
+	  state.regression.slope.deviation = payload.slope.deviation;
+	  state.regression.slope.plusMinus = payload.slope.plusMinus;
 
-	  state.regression.intercept.value = payload[3][0];
-	  state.regression.intercept.deviation = payload[4][0];
-	  state.regression.intercept.plusMinus = payload[5][0];
+	  state.regression.intercept.value = payload.intercept.value;
+	  state.regression.intercept.deviation = payload.intercept.deviation;
+	  state.regression.intercept.plusMinus = payload.intercept.plusMinus;
+	},
 
-    state.figuresOfMerit.sensitivity = Math.abs(state.regression.slope.value);
-    state.figuresOfMerit.gamma = payload[8][0];
-    state.figuresOfMerit.oneOverGamma = 1 / state.figuresOfMerit.gamma;
-    state.figuresOfMerit.decisionLimit = payload[9][0];
-    state.figuresOfMerit.detectionLimit = payload[10][0];
-    state.figuresOfMerit.quantitationLimit = payload[11][0];
-    state.figuresOfMerit.correlationCoefficient = payload[12][0];
+	updateFiguresOfMerit (state, payload) {
+    state.figuresOfMerit.sensitivity = payload.sensitivity;
+    state.figuresOfMerit.gamma = payload.gamma;
+    state.figuresOfMerit.oneOverGamma = payload.oneOverGamma;
+    state.figuresOfMerit.decisionLimit = payload.decisionLimit;
+    state.figuresOfMerit.detectionLimit = payload.detectionLimit;
+    state.figuresOfMerit.quantitationLimit = payload.quantitationLimit;
+    state.figuresOfMerit.correlationCoefficient = payload.correlationCoefficient;
 	}
 
 };
@@ -88,13 +92,8 @@ const mutations = {
 const actions = {
 
 	updateCalibrationValues({ commit, dispatch }, newCalibrationSamples) {
-    let analytes : Number[] = [], signals : Number[] = [];
-    for (let row = 0; row < newCalibrationSamples.length; row++) {
-      for (let col = 1; col < newCalibrationSamples[row].length; col++) {
-        analytes.push(newCalibrationSamples[row][0]);
-        signals.push(newCalibrationSamples[row][col]);
-      }
-    }
+    let [analytes, signals] = createDataPoints(newCalibrationSamples);
+
 		commit('updateCalibrationValues', {
 		  newCalibrationSamples: newCalibrationSamples,
 		  newAnalytes: analytes,
@@ -102,7 +101,7 @@ const actions = {
 		});
     dispatch('getNewPlotUrl');
     dispatch('updateGeneralInfo', newCalibrationSamples);
-    dispatch('getRegressionValues');
+    dispatch('getCalibrationResults');
 	},
 
 	getNewPlotUrl ({ commit }) {
@@ -119,27 +118,48 @@ const actions = {
 	},
 
 	updateGeneralInfo ({ commit }, newCalibrationSamples) {
-    const concentrationLevels = state.calibrationSamples.length;
-
-    let replicates = 0, dataPoints = 0;
-    for (let row = 0; row < newCalibrationSamples.length; row++) {
-      replicates = Math.max(replicates, state.calibrationSamples[row].length - 1);
-      dataPoints += (state.calibrationSamples[row].length - 1);
-    }
+	  let [newConcentrationLevels,
+	    newReplicates,
+	    newDataPoints
+	  ] = generalInfo(newCalibrationSamples);
 
     commit('updateGeneralInfo', {
-      concentrationLevels: concentrationLevels,
-      replicates: replicates,
-      dataPoints: dataPoints
+      concentrationLevels: newConcentrationLevels,
+      replicates: newReplicates,
+      dataPoints: newDataPoints
     });
 	},
 
-	getRegressionValues ({ commit } ) {
+	getCalibrationResults ({ commit } ) {
     axios.post('https://atmunr.ocpu.io/UNIVAR_EJCR_R-API/R/fitSimpleLinearRegressionOLS/json', {
       x: state.dataPointsAnalytes, y: state.dataPointsSignals
     })
     .then((response) => {
-      commit('updateRegressionValues', response.data);
+      const newRegressionValues = {
+        slope: {
+          value: response.data[0][0],
+          deviation: response.data[1][0],
+          plusMinus: response.data[2][0]
+        },
+        intercept: {
+          value: response.data[3][0],
+          deviation: response.data[4][0],
+          plusMinus: response.data[5][0]
+        }
+      };
+
+      const newFiguresOfMerit = {
+        sensitivity: Math.abs(newRegressionValues.slope.value),
+        gamma: response.data[8][0],
+        oneOverGamma: 1 / response.data[8][0],
+        decisionLimit: response.data[9][0],
+        detectionLimit: response.data[10][0],
+        quantitationLimit: response.data[11][0],
+        correlationCoefficient: response.data[12][0],
+      };
+
+      commit('updateRegressionValues', newRegressionValues);
+      commit('updateFiguresOfMerit', newFiguresOfMerit);
     })
     .catch((error) => { console.log(error); });
 	}
---
+++ src/store/utils.ts
@@ -0,0 +1,30 @@
+
+export function createDataPoints (samples : Number[][]) {
+
+  let analytes : Number[] = [], signals : Number[] = [];
+
+  for (let row = 0; row < samples.length; row++) {
+    for (let col = 1; col < samples[row].length; col++) {
+      analytes.push(samples[row][0]);
+      signals.push(samples[row][col]);
+    }
+  }
+
+  return [analytes, signals];
+}
+
+export function generalInfo (samples: Number[][]) {
+  const concentrationLevels = samples.length;
+
+  let replicates = 0, dataPoints = 0;
+  for (let row = 0; row < samples.length; row++) {
+    replicates = Math.max(replicates, samples[row].length - 1);
+    dataPoints += (samples[row].length - 1);
+  }
+
+  return [concentrationLevels, replicates, dataPoints];
+}
+
+export default {
+	createDataPoints, generalInfo
+};
